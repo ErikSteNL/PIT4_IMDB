fa6d87ff1febd8ab7a6a3180b5661a477d7e682f
Leyundai <1406221kremer@zuyd.nl>
2016-06-01 11:12:31 +0200

    Custom namespaces toegevoegd
    DBModel.bu

--- PIT4_IMDB/PIT4_IMDB/DBModel.Designer.cs
+++ PIT4_IMDB/PIT4_IMDB/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Dropbox\PIT4\Code\PIT4_IMDB\PIT4_IMDB\DBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Leonardo\Documents\PIT4_Leonardo_Github\PIT4_IMDB\PIT4_IMDB\PIT4_IMDB\DBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
